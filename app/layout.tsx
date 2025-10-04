3bc060c57ad84b1239f6907ad44b099159d2dc1a
Roni Sarkar <ronisarkar742202@gmail.com>
2025-10-04 14:04:57 +0530

    minor fixes

--- app/layout.tsx
+++ app/layout.tsx
@@ -9,52 +9,9 @@ import PwaInstall from '@/components/PwaInstall';
 import IosAddToHome from '@/components/IosAddToHome';
 import Navbar from '@/components/Navbar';
 import { ThemeProvider } from '@/components/theme-provider';
+import { Analytics } from '@vercel/analytics/next';
 
-const workSans = localFont({
-	src: [
-		{
-			path: './fonts/WorkSans-Black.ttf',
-			weight: '900',
-			style: 'normal',
-		},
-		{
-			path: './fonts/WorkSans-ExtraBold.ttf',
-			weight: '800',
-			style: 'normal',
-		},
-		{
-			path: './fonts/WorkSans-Bold.ttf',
-			weight: '700',
-			style: 'normal',
-		},
-		{
-			path: './fonts/WorkSans-SemiBold.ttf',
-			weight: '600',
-			style: 'normal',
-		},
-		{
-			path: './fonts/WorkSans-Medium.ttf',
-			weight: '500',
-			style: 'normal',
-		},
-		{
-			path: './fonts/WorkSans-Regular.ttf',
-			weight: '400',
-			style: 'normal',
-		},
-		{
-			path: './fonts/WorkSans-Light.ttf',
-			weight: '200',
-			style: 'normal',
-		},
-		{
-			path: './fonts/WorkSans-ExtraLight.ttf',
-			weight: '100',
-			style: 'normal',
-		},
-	],
-	variable: '--font-work-sans',
-});
+
 
 export const metadata: Metadata = {
 	title: 'Create Next App',
@@ -98,18 +55,19 @@ export default function RootLayout({
 					href={`${process.env.NEXT_PUBLIC_SITE_URL}/rss.xml`}
 				/>
 			</head>
-			<body >
+			<body>
 				<ThemeProvider
 					attribute="class"
 					defaultTheme="system"
 					enableSystem
 					disableTransitionOnChange>
-				<Navbar />
-				{children}
-				<PwaInstall />
-				<IosAddToHome />
-				<Toaster position="top-right" />
+					<Navbar />
+					{children}
+					<PwaInstall />
+					<IosAddToHome />
+					<Toaster position="top-right" />
 				</ThemeProvider>
+				<Analytics />
 			</body>
 		</html>
 	);
